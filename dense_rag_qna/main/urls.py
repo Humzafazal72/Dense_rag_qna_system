6d8cab65a07aef8529001a62e88bdc8b4759c2f7
humzafazal <abbasisaud99@gmail.com>
2024-12-15 19:28:51 +0500

    added requirements.txt and fixed minor issue in urls.py

--- dense_rag_qna/main/urls.py
+++ dense_rag_qna/main/urls.py
@@ -3,5 +3,5 @@ from .views import document_upload,retrieve_information
 
 urlpatterns = [
     path('add_document',document_upload,name='add_document'),
-    path('get_response',retrieve_information,name='get_response'),
+    path('get_response/<str:id>',retrieve_information,name='get_response'),
 ]
