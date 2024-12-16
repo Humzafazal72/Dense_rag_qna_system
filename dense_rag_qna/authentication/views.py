168ef92c3e83d177d93aee6e7a412c3695a76a27
M-Ali-Haider <mah01134212100@gmail.com>
2024-12-16 11:39:12 +0500

    updated views.py

--- dense_rag_qna/authentication/views.py
+++ dense_rag_qna/authentication/views.py
@@ -3,51 +3,49 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth import login,logout,aauthenticate
+from django.contrib.auth import login,logout,authenticate
 
 @csrf_exempt
 def signup(request):
     if request.method == "POST":
         data = json.loads(request.body)
-
         email = data.get('email')
         username = data.get('username')
         password = data.get('password')
-        
+
         if not password or not email:
-                return JsonResponse({"error": "Name and email are required."}, status=400)
-        
+            return JsonResponse({"error": "Name and email are required."}, status=400)
+
         user = User.objects.create_user(username=username,email=email,password=password)
         user.save()
-        
         return JsonResponse({"success": "User signed-up successfully"}, status=200)
-        
+
     else:
         return JsonResponse({"error": "Invalid HTTP method."}, status=405)
-    
+
+@csrf_exempt
 def signin(request):
     if request.method == "POST":
         data = json.loads(request.body)
-
-        username = data.get('username')
+        
+        username = data.get('username')     
         password = data.get('password')
 
         if not password or not username:
-                return JsonResponse({"error": "Name and email are required."}, status=400)
-        
-        user = aauthenticate(request,username,password)
+            return JsonResponse({"error": "Name and email are required."}, status=400)
 
+        user = authenticate(username=username,password=password)
         if user is not None:
             login(request,user)
             return JsonResponse({'success':'User signed-in successfully'},status=200)
-        
         else:
-            return JsonResponse({'error':'Incorrect password or username'},status=404) 
-    
+            return JsonResponse({'error':'Incorrect password or username'},status=404)
     else:
         return JsonResponse({"error": "Invalid HTTP method."}, status=405)
-    
+
+  
+
+@csrf_exempt 
 def signout(request):
     logout(request)
     return JsonResponse({'success':'User logged-out successfully'},status=200)
-        
--- dense_rag_qna/main/views.py
+++ dense_rag_qna/main/views.py
@@ -4,61 +4,60 @@ import pickle
 from pathlib import Path
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .utils import create_db,retrieve_docs,optimization_chain,generation_chain
-
-CURR_DIR = Path(__file__).resolve().parent
-CACHE_PATH = os.path.join(Path(__file__).resolve().parent,'cache')
-PERSISTENT_DIR = os.path.join(Path(__file__).resolve().parent,'Vector_DB')
-CHAT_HISTORY_DIR = os.path.join(Path(__file__).resolve().parent,'chat_history')
-
-@csrf_exempt
+from .utils import create_db, retrieve_docs, optimization_chain, generation_chain
+  
+CURR_DIR = Path(__file__).resolve().parent  
+CACHE_PATH = os.path.join(Path(__file__).resolve().parent, 'cache')  
+PERSISTENT_DIR = os.path.join(Path(__file__).resolve().parent, 'Vector_DB')  
+CHAT_HISTORY_DIR = os.path.join(Path(__file__).resolve().parent, 'chat_history')  
+  
+@csrf_exempt  
 def document_upload(request):
     if request.method == 'POST':
         document = request.FILES.get("document")
-        doc_name = document.name
-
-        doc_path = os.path.join(CURR_DIR,'Documents',doc_name)
-
+        doc_name = document.name.replace('.pdf', '')
+  
+        doc_path = os.path.join(CURR_DIR, 'Documents', doc_name)
+  
         if not document:
             return JsonResponse({"error": "Document not uploaded."}, status=400)
-
+  
         with open(doc_path, "wb+") as destination:
             for chunk in document.chunks():
                 destination.write(chunk)
         
-        create_db(doc_path,CACHE_PATH,doc_name,PERSISTENT_DIR)
-
-        return JsonResponse({'success':'Vector Database created successfully'},status=200)
+        create_db(doc_path, CACHE_PATH, doc_name, PERSISTENT_DIR)
+  
+        return JsonResponse({'success': 'Vector Database created successfully'}, status=200)
     else:
         return JsonResponse({"error": "Invalid HTTP method."}, status=405)
-
-@csrf_exempt
-def retrieve_information(request,doc_name):
+  
+@csrf_exempt  
+def retrieve_information(request, doc_name):
     if request.method == 'POST':
-
         if os.path.exists(f"{CHAT_HISTORY_DIR}/{doc_name.split('.')[0]}.pkl"):
-            with open(f"{CHAT_HISTORY_DIR}/{doc_name.split('.')[0]}.pkl",'rb') as file:
+            with open(f"{CHAT_HISTORY_DIR}/{doc_name.split('.')[0]}.pkl", 'rb') as file:
                 chat_history = pickle.load(file)
         else:
             chat_history = []
-
+  
         data = json.loads(request.body)
-
+  
         u_query = data.get('query')
-
+  
         o_query = optimization_chain.invoke(u_query)
-
-        similar_docs = retrieve_docs(doc_name,o_query,CACHE_PATH,PERSISTENT_DIR)
-
-        response = generation_chain.invoke({'question':u_query,'context':similar_docs})
-
+  
+        similar_docs = retrieve_docs(doc_name.replace('.pdf', ''), o_query, CACHE_PATH, PERSISTENT_DIR)
+  
+        response = generation_chain.invoke({'question': u_query, 'context': similar_docs})
+        print(similar_docs)
+  
         chat_history.append(u_query)
         chat_history.append(response.content)
-
-        with open(f"{CHAT_HISTORY_DIR}/{doc_name.split('.')[0]}.pkl",'wb') as file:
-                chat_history = pickle.dump(chat_history,file)
-
-        return JsonResponse({"response":response.content},status=205)
-    
+  
+        with open(f"{CHAT_HISTORY_DIR}/{doc_name.split('.')[0]}.pkl", 'wb') as file:
+                pickle.dump(chat_history, file)
+  
+        return JsonResponse({"response": response.content, 'o_query': o_query}, status=205)
     else:
         return JsonResponse({"error": "Invalid HTTP method."}, status=405)
