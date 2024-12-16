c1412a06021b00a525a62341706aababbda9179b
humzafazal <abbasisaud99@gmail.com>
2024-12-16 19:41:35 +0500

    made changes to views.py

--- dense_rag_qna/authentication/views.py
+++ dense_rag_qna/authentication/views.py
@@ -1,9 +1,13 @@
+import os
 import json
+from django.conf import settings
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import login,logout,authenticate
+
+(settings.BASE_DIR / 'main' / 'users_data').mkdir(exist_ok = True)
 
 @csrf_exempt
 def signup(request):
@@ -13,12 +17,16 @@ def signup(request):
         username = data.get('username')
         password = data.get('password')
 
-        if not password or not email:
-            return JsonResponse({"error": "Name and email are required."}, status=400)
+        if not password or not username:
+            return JsonResponse({"error": "Name and username are required."}, status=400)
 
-        user = User.objects.create_user(username=username,email=email,password=password)
+        user = User.objects.create_user(username=username, email=email, password=password)
         user.save()
-        return JsonResponse({"success": "User signed-up successfully"}, status=200)
+
+        (settings.BASE_DIR / 'main' / 'users_data' / username).mkdir()
+
+        return JsonResponse({"success": "User signed-up successfully", "docs":[],
+                             "username":username, "email":email}, status=200)
 
     else:
         return JsonResponse({"error": "Invalid HTTP method."}, status=405)
@@ -37,13 +45,15 @@ def signin(request):
         user = authenticate(username=username,password=password)
         if user is not None:
             login(request,user)
-            return JsonResponse({'success':'User signed-in successfully'},status=200)
+
+            docs_path = settings.BASE_DIR / 'main' / 'users_data' / username 
+            docs = os.listdir(docs_path)
+            
+            return JsonResponse({'success':'User signed-in successfully','docs':docs},status=200)
         else:
             return JsonResponse({'error':'Incorrect password or username'},status=404)
     else:
         return JsonResponse({"error": "Invalid HTTP method."}, status=405)
-
-  
 
 @csrf_exempt 
 def signout(request):
--- dense_rag_qna/main/views.py
+++ dense_rag_qna/main/views.py
@@ -5,19 +5,24 @@ from pathlib import Path
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .utils import create_db, retrieve_docs, optimization_chain, generation_chain
-  
+
 CURR_DIR = Path(__file__).resolve().parent  
-CACHE_PATH = os.path.join(Path(__file__).resolve().parent, 'cache')  
-PERSISTENT_DIR = os.path.join(Path(__file__).resolve().parent, 'Vector_DB')  
-CHAT_HISTORY_DIR = os.path.join(Path(__file__).resolve().parent, 'chat_history')  
-  
+(CURR_DIR / 'cache').mkdir(exist_ok=True)
+(CURR_DIR / 'users_data').mkdir(exist_ok=True)
+
+CACHE_PATH = os.path.join(Path(__file__).resolve().parent, 'cache')    
+
 @csrf_exempt  
 def document_upload(request):
     if request.method == 'POST':
         document = request.FILES.get("document")
-        doc_name = document.name.replace('.pdf', '')
-  
-        doc_path = os.path.join(CURR_DIR, 'Documents', doc_name)
+        doc_name = document.name
+        doc_name_ = doc_name.replace('.pdf', '')
+        username = str(request.user.username)
+        chat_history = []
+
+        (CURR_DIR / 'users_data' / username / doc_name_).mkdir(exist_ok = True)
+        doc_path = CURR_DIR / 'users_data' / username / doc_name_ / doc_name
   
         if not document:
             return JsonResponse({"error": "Document not uploaded."}, status=400)
@@ -26,17 +31,28 @@ def document_upload(request):
             for chunk in document.chunks():
                 destination.write(chunk)
         
-        create_db(doc_path, CACHE_PATH, doc_name, PERSISTENT_DIR)
+        (CURR_DIR / 'users_data' / username / doc_name_ / 'Persistent').mkdir(exist_ok = True)
+
+        PERSISTENT_DIR = os.path.join(CURR_DIR, 'users_data', username, doc_name_, 'Persistent')
+        
+        create_db(doc_path, CACHE_PATH, doc_name_, PERSISTENT_DIR)
+
+        docs = os.listdir(os.path.join(CURR_DIR, 'users_data', username))
   
-        return JsonResponse({'success': 'Vector Database created successfully'}, status=200)
+        return JsonResponse({'success': 'Vector Database created successfully', 'docs':docs, 'chat_history':[]}, status=200)
     else:
         return JsonResponse({"error": "Invalid HTTP method."}, status=405)
   
 @csrf_exempt  
 def retrieve_information(request, doc_name):
     if request.method == 'POST':
-        if os.path.exists(f"{CHAT_HISTORY_DIR}/{doc_name.split('.')[0]}.pkl"):
-            with open(f"{CHAT_HISTORY_DIR}/{doc_name.split('.')[0]}.pkl", 'rb') as file:
+        doc_name = doc_name.replace('.pdf','')
+        username = str(request.user.username)
+
+        chat_history_path = CURR_DIR / 'users_data' / username / doc_name / 'chat_history.pkl'
+
+        if chat_history_path.exists():
+            with chat_history_path.open('rb') as file:  # Use Pathlib's open method
                 chat_history = pickle.load(file)
         else:
             chat_history = []
@@ -44,20 +60,33 @@ def retrieve_information(request, doc_name):
         data = json.loads(request.body)
   
         u_query = data.get('query')
-  
         o_query = optimization_chain.invoke(u_query)
-  
-        similar_docs = retrieve_docs(doc_name.replace('.pdf', ''), o_query, CACHE_PATH, PERSISTENT_DIR)
+
+        PERSISTENT_DIR = os.path.join(CURR_DIR, 'users_data', username, doc_name, 'Persistent')  
+        similar_docs = retrieve_docs(doc_name, o_query, CACHE_PATH, PERSISTENT_DIR)
   
         response = generation_chain.invoke({'question': u_query, 'context': similar_docs})
-        print(similar_docs)
   
-        chat_history.append(u_query)
-        chat_history.append(response.content)
+        chat = {'user':u_query,'response':response.content}
+        chat_history.append(chat)
   
-        with open(f"{CHAT_HISTORY_DIR}/{doc_name.split('.')[0]}.pkl", 'wb') as file:
+        with chat_history_path.open('wb') as file:
                 pickle.dump(chat_history, file)
   
-        return JsonResponse({"response": response.content, 'o_query': o_query}, status=205)
+        return JsonResponse({"response": response.content, 'chat_history': chat_history}, status=205)
     else:
         return JsonResponse({"error": "Invalid HTTP method."}, status=405)
+    
+def get_chat_history(request,doc_name):
+    username = request.user.username
+    doc_name = doc_name.split('.pdf','')
+
+    chat_history_path = CURR_DIR / 'users_data' / username / doc_name / 'chat_history.pkl'
+
+    if chat_history_path.exists():
+        with chat_history_path.open('rb') as file:  # Use Pathlib's open method
+            chat_history = pickle.load(file)
+        
+        return JsonResponse({'success':'chat history retreived successfully',"chat_history":chat_history}, status = 205)
+    else:
+        return JsonResponse({"error":"No chat history for the document {doc_name}"})
